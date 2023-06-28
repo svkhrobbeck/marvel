1b8d3d0c04f1b4c5e5b1319a0befef674d8a9146
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-28 22:45:17 +0500

    Add characters page component to router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 
 // pages
-import { Home } from "../pages";
+import { Characters, Home } from "../pages";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Characters />} />
     </Routes>
   );
 };
