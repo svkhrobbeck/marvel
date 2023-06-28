0e894e1f735bc120039df9d9f1c0c78f6c9aed39
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-28 22:49:58 +0500

    Create constants

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const pages = [
+  { page: "Characters", path: "/" },
+  { page: "Comics", path: "/comics" },
+  { page: "Creators", path: "/creators" },
+  { page: "Events", path: "/events" },
+  { page: "Series", path: "/series" },
+  { page: "Stories", path: "/stories" },
+];
