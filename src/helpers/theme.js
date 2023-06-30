eaf7f52031c68b5f7af6bdd68e90745f02ccadf2
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 01:44:05 +0500

    Create theme helper for mui

---
+++ src/helpers/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Inter", "Arial", "sans-serif"].join(","),
+  },
+});
+
+export default theme;
