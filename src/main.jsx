b0e552e450cab67c994008ccc89fdb6883eaa300
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 01:46:38 +0500

    Add theme-provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./services/queries.js";
 import "./index.scss";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./helpers/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
