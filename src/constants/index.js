3ba7f0a0969fb65754dc2961e8a3e4710dde5ee2
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 01:46:00 +0500

    Add API_TIME_STAMP to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,3 +10,5 @@ export const pages = [
 export const BASE_API_URL = "https://gateway.marvel.com/v1/public/";
 
 export const API_HASH = "6c3306276a5ce981ec8e4804e80dd204";
+
+export const API_TIME_STAMP = 168813781091;
