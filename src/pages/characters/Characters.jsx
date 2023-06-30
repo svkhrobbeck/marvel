66e59946c975cdb658dcfd538696c01e05ecce7f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 01:46:56 +0500

    Change and complete characters page component

--- src/pages/characters/Characters.jsx
+++ src/pages/characters/Characters.jsx
@@ -1,5 +1,23 @@
+import { CharacterCard } from "../../components";
+import { useCharacters } from "./queries";
+import { LinearProgress, Alert, Grid } from "@mui/material";
+
 const Characters = () => {
-  return <div>Characters</div>;
+  const { isError, isLoading, error, data } = useCharacters({ limit: 20, offset: 0 });
+
+  if (isLoading) return <LinearProgress />;
+
+  if (isError) return <Alert severity="error">{error.toString()}</Alert>;
+
+  return (
+    <Grid container py={"30px"} maxWidth={{ sm: "98%", lg: "95%" }} mx={"auto"} alignItems={"stretch"} spacing={2}>
+      {data?.results.map(result => (
+        <Grid key={result.id} item display={"flex"} xs={12} md={6} lg={4} xl={3}>
+          <CharacterCard {...result} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Characters;
