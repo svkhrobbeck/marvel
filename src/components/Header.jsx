68b4b7c32ff9c948fdec5e58f01dc346c27747b4
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 01:44:48 +0500

    Optimize and make sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import { Menu as MenuIcon } from "@mui/icons-material";
 import { pages } from "../constants";
@@ -19,61 +19,66 @@ const Header = () => {
   };
 
   return (
-    <header className="site-header">
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: "flex" }}>
+    <AppBar position="sticky" sx={{ zIndex: 100 }}>
+      <Container sx={{ maxWidth: { sm: "98%", lg: "95%" } }}>
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: "flex" }}>
+            <Link to="/">
               <img height={40} src="/logo.svg" alt="marvel logo" />
-            </Box>
+            </Link>
+          </Box>
 
-            <Box sx={{ display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map(({ page, path }) => (
-                  <MenuItem selected={pathname === path} key={page} onClick={() => handleCloseNavMenu(path)}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
               {pages.map(({ page, path }) => (
-                <Button key={page} disabled={pathname === path} onClick={() => handleCloseNavMenu(path)} sx={{ my: 2, color: "white", display: "block" }}>
-                  {page}
-                </Button>
+                <MenuItem selected={pathname === path} key={page} onClick={() => handleCloseNavMenu(path)}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
               ))}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </header>
+            </Menu>
+          </Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {pages.map(({ page, path }) => (
+              <Button
+                key={page}
+                disabled={pathname === path}
+                onClick={() => handleCloseNavMenu(path)}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 export default Header;
