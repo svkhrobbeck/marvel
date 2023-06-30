2752183fa3021a37ac6cd473b57e9bf59fb52f7c
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 01:44:30 +0500

    Optimize route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 // pages
-import { Characters, Home } from "../pages";
+import { Character, Characters } from "../pages";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Characters />} />
+      <Route path="/" element={<Navigate to="/characters" />} />
+      <Route path="/characters" element={<Characters />} />
+      <Route path="/characters/:id" element={<Character />} />
     </Routes>
   );
 };
