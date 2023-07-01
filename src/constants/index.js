94bc657c10e028b4c6155d5ecc8b34a9aa437da3
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 13:59:13 +0500

    Optimize

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,6 @@ export const pages = [
 ];
 
 export const BASE_API_URL = "https://gateway.marvel.com/v1/public/";
-
 export const API_HASH = "6c3306276a5ce981ec8e4804e80dd204";
-
 export const API_TIME_STAMP = 168813781091;
+export const PAGINATION_LIMIT = 20;
