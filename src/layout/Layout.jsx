6695c2eeb7f8ad8897d69b90ae75f41f94682742
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 14:17:44 +0500

    Add container as global

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,13 +5,16 @@ import { Footer, Header } from "../components";
 
 // router
 import Router from "../router/Router";
+import { Box } from "@mui/material";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <main className="main-content">
-        <Router />
+        <Box mx="auto" py="30px" maxWidth={{ sm: "98%", lg: "95%" }}>
+          <Router />
+        </Box>
       </main>
       <Footer />
     </>
