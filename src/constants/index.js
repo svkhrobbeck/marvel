40a367e4c8da1231352cfa78ac05728b35496414
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 14:11:33 +0500

    Add changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,9 +2,7 @@ export const pages = [
   { page: "Characters", path: "/characters" },
   { page: "Comics", path: "/comics" },
   { page: "Creators", path: "/creators" },
-  { page: "Events", path: "/events" },
   { page: "Series", path: "/series" },
-  { page: "Stories", path: "/stories" },
 ];
 
 export const BASE_API_URL = "https://gateway.marvel.com/v1/public/";
