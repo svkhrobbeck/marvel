139bbe21293de46da74b07d667d2577a826dc516
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 14:00:39 +0500

    Optimize character-card

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -5,7 +5,7 @@ export default function RecipeReviewCard({ id, name, thumbnail }) {
   const navigate = useNavigate();
 
   return (
-    <Card sx={{ maxWidth: { sm: "800px", md: "600px", xl: "400px" }, width: "100%", display: "flex", flexDirection: "column" }}>
+    <Card sx={{ maxWidth: { sm: "800px", md: "600px", xl: "500px" }, width: "100%", display: "flex", flexDirection: "column" }}>
       <CardMedia sx={{ height: { sm: "350px", md: "300px" } }} image={thumbnail.path + "/landscape_incredible.jpg"} title={name} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,5 @@
 export { default as CharacterCard } from "./CharacterCard";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
+export { default as FilterBar } from "./FilterBar";
+export { default as CharactersList } from "./CharactersList";
