6d3c74e5414f60511c587f0df5838467364869e0
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 14:18:50 +0500

    Add new pages to router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 // pages
-import { Character, Characters } from "../pages";
+import { Character, Characters, Comics, Creators, Series } from "../pages";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/characters" />} />
       <Route path="/characters" element={<Characters />} />
+      <Route path="/comics" element={<Comics />} />
+      <Route path="/creators" element={<Creators />} />
+      <Route path="/series" element={<Series />} />
       <Route path="/characters/:id" element={<Character />} />
     </Routes>
   );
